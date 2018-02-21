0b919e2cd02c64d6338f75a78895a19ee645bec5
Legaspi <legabit@gmail.com>
2018-02-21 11:46:21 -0600

    server bueno

---
+++ parcial1/basic-88/node.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var PORT = 8082;
+
+var app=express()
+app.get('/', function(req,res)
+ {
+   res.sendfile('index.html');
+ }
+);
+
+
+
+app.listen(PORT);
+console.log('Running on port ' + PORT);
--- parcial1/basic-88/server.js
+++ parcial1/basic-88/server.js
@@ -1,14 +1,18 @@
-var express = require('express')
-var PORT = 8082;
+var express = require('express');
+var path =  require('path');
+
+var PORT = 8080;
 
 var app=express()
-app.get('/', function(req,res)
- {
-   res.sendfile('index.html');
- }
-);
 
+app.use('/styles', express.static(path.join(__dirname, './styles')));
+app.use('/scripts', express.static(path.join(__dirname, './scripts')));
+app.use('/images', express.static(path.join(__dirname, './images')));
 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(PORT);
+
 console.log('Running on port ' + PORT);
